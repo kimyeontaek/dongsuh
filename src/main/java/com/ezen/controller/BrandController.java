4e68e707a40eb932ec5697e3c67167cb216736bd
LPS <LPS@DESKTOP-KGOL2UR>
2019-04-10 18:06:46 +0900

    test1

--- src/main/java/com/ezen/controller/BrandController.java
+++ src/main/java/com/ezen/controller/BrandController.java
@@ -5,8 +5,7 @@ import java.util.HashMap;
 
 import javax.servlet.http.HttpSession;
 
-import net.sf.json.JSONArray;
-import net.sf.json.JSONObject;
+
 
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,19 +17,19 @@ import org.springframework.web.servlet.ModelAndView;
 
 import dongsuh.dao.BrandDAO;
 import dongsuh.vo.BrandVO;
-
+//ddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd
 @Controller
 public class BrandController {
 	@Autowired
 	SqlSessionTemplate sqlSession;
 
-	/* 브랜드 선택 페이지로 이동 */
+	/* 釉뚮옖�뱶 �꽑�깮 �럹�씠吏�濡� �씠�룞 */
 	@RequestMapping(value = "/brand.do", method = RequestMethod.GET)
 	public ModelAndView brand() {
 		ModelAndView mv = new ModelAndView();
 
 		BrandDAO dao = sqlSession.getMapper(BrandDAO.class);
-		// 브랜드 이미지 보여주기
+		// 釉뚮옖�뱶 �씠誘몄� 蹂댁뿬二쇨린
 		ArrayList<BrandVO> list = dao.getMainList();
 
 		mv.addObject("list", list);
@@ -39,15 +38,15 @@ public class BrandController {
 
 	}
 
-	/* 브랜드 이미지 선택시 해당 브랜드 주문페이지로 이동 */
+	/* 釉뚮옖�뱶 �씠誘몄� �꽑�깮�떆 �빐�떦 釉뚮옖�뱶 二쇰Ц�럹�씠吏�濡� �씠�룞 */
 	@RequestMapping(value = "/brandSal.do", method = RequestMethod.GET)
 	public ModelAndView brandSal(String brandname) {
 		ModelAndView mv = new ModelAndView();
 
 		BrandDAO dao = sqlSession.getMapper(BrandDAO.class);
-		// 브랜드 주문페이지 보여주기
+		// 釉뚮옖�뱶 二쇰Ц�럹�씠吏� 蹂댁뿬二쇨린
 		BrandVO vo = dao.getResultVO(brandname);
-		// 상품목록 보여주기
+		// �긽�뭹紐⑸줉 蹂댁뿬二쇨린
 		ArrayList<BrandVO> list = dao.getItemList(brandname);
 
 		mv.addObject("brandname", brandname);
@@ -58,7 +57,7 @@ public class BrandController {
 		return mv;
 	}
 
-	/* 바로구매하기 버튼 눌렀을때 이동하는 페이지 */
+	/* 諛붾줈援щℓ�븯湲� 踰꾪듉 �닃���쓣�븣 �씠�룞�븯�뒗 �럹�씠吏� */
 	@RequestMapping(value = "/order.do", method = RequestMethod.POST)
 	public ModelAndView order(BrandVO vo) {
 		ModelAndView mv = new ModelAndView();
@@ -67,19 +66,19 @@ public class BrandController {
 		int result = 0;
 
 		for (int i = 0; i < vo.getNames().length; i++) {
-			// 바로구매하기 테이블에 상품목록 저장
+			// 諛붾줈援щℓ�븯湲� �뀒�씠釉붿뿉 �긽�뭹紐⑸줉 ���옣
 			result = dao.getInsertOrderResult(vo.getNames()[i],
 					vo.getNums()[i], vo.getPrices()[i]);
 		}
-		// 바로구매하기 목록 보여주기
+		// 諛붾줈援щℓ�븯湲� 紐⑸줉 蹂댁뿬二쇨린
 		ArrayList<BrandVO> list = dao.getOrderListResult();
 
-		// 총 상품금액 계산
+		// 珥� �긽�뭹湲덉븸 怨꾩궛
 		int sum = 0;
 		for (int i = 0; i < vo.getPrices().length; i++) {
 			sum = (vo.getPrices()[i] * vo.getNums()[i]) + sum;
 		}
-		// 바로구매하기 테이블 삭제 -> 바로구매하기는 임시로 저장하여 리스트만 출력
+		// 諛붾줈援щℓ�븯湲� �뀒�씠釉� �궘�젣 -> 諛붾줈援щℓ�븯湲곕뒗 �엫�떆濡� ���옣�븯�뿬 由ъ뒪�듃留� 異쒕젰
 		dao.getOrderTruncateResult();
 		if (result == 1) {
 			mv.addObject("list", list);
@@ -89,7 +88,7 @@ public class BrandController {
 		return mv;
 	}
 
-	/* 장바구니추가 버튼을 눌럿을때 이동하는 페이지 */
+	/* �옣諛붽뎄�땲異붽� 踰꾪듉�쓣 �닃�읉�쓣�븣 �씠�룞�븯�뒗 �럹�씠吏� */
 	@RequestMapping(value = "/basket_insert.do", method = RequestMethod.POST)
 	public ModelAndView basket_insert(BrandVO vo, HttpSession session) {
 		ModelAndView mv = new ModelAndView();
@@ -97,14 +96,14 @@ public class BrandController {
 		BrandDAO dao = sqlSession.getMapper(BrandDAO.class);
 		int result = 0;
 		for (int i = 0; i < vo.getBaname().length; i++) {
-			// 장바구니 테이블에 상품목록 저장
+			// �옣諛붽뎄�땲 �뀒�씠釉붿뿉 �긽�뭹紐⑸줉 ���옣
 			result = dao.getInsertBasketResult(sid, vo.getBaname()[i],
 					vo.getBanum()[i], vo.getBaprice()[i]);
 		}
-		// 바로구매하기 목록 보여주기
+		// 諛붾줈援щℓ�븯湲� 紐⑸줉 蹂댁뿬二쇨린
 		ArrayList<BrandVO> list = dao.getBasketListResult(sid);
 
-		// 총 상품금액 계산
+		// 珥� �긽�뭹湲덉븸 怨꾩궛
 		int sum = 0;
 		for (BrandVO arr : list) {
 			sum = (arr.getPrice() * arr.getNum()) + sum;
@@ -117,7 +116,7 @@ public class BrandController {
 		return mv;
 	}
 
-	/* 장바구니 페이지로 이동 */
+	/* �옣諛붽뎄�땲 �럹�씠吏�濡� �씠�룞 */
 	@RequestMapping(value = "/basket.do", method = RequestMethod.GET)
 	public ModelAndView basket(HttpSession session) {
 		ModelAndView mv = new ModelAndView();
@@ -125,9 +124,9 @@ public class BrandController {
 		int sum = 0;
 
 		BrandDAO dao = sqlSession.getMapper(BrandDAO.class);
-		// 장바구니 보여주기
+		// �옣諛붽뎄�땲 蹂댁뿬二쇨린
 		ArrayList<BrandVO> list = dao.getBasketListResult(sid);
-		// 총 판매액 계산
+		// 珥� �뙋留ㅼ븸 怨꾩궛
 		for (BrandVO arr : list) {
 			sum = (arr.getPrice() * arr.getNum()) + sum;
 		}
@@ -139,36 +138,15 @@ public class BrandController {
 		return mv;
 	}
 
-	/* 상품평 등록 기능 */
+	/* �긽�뭹�룊 �벑濡� 湲곕뒫 */
 	@RequestMapping(value = "/brandSalReply.do", method = RequestMethod.GET)
 	@ResponseBody
 	public String brandSalReply(BrandVO vo) {
 		BrandDAO dao = sqlSession.getMapper(BrandDAO.class);
-		// 상품평 등록
+		// �긽�뭹�룊 �벑濡�
 		int result = dao.getInsertResult(vo);
 		return String.valueOf(result);
 	}
 
-	/* 상품평 목록 갱신 */
-	@RequestMapping(value = "/brandSalReply_list.do", method = RequestMethod.GET)
-	@ResponseBody
-	public JSONArray brandSalReply_list(String brandname) {
-		JSONArray jarray = new JSONArray();
-		BrandDAO dao = sqlSession.getMapper(BrandDAO.class);
-		// 상품평 목록 가져오기
-		ArrayList<BrandVO> list = dao.getReplyList(brandname);
 
-		/* JSONArray를 이용 */
-		for (BrandVO vo : list) {
-			JSONObject obj = new JSONObject();
-			obj.put("brandname", brandname);
-			obj.put("rno", vo.getRno());
-			obj.put("content", vo.getContent());
-			obj.put("writer", vo.getWriter());
-			obj.put("rdate", vo.getRdate());
-			// JSONArray에 입력 후 전달
-			jarray.add(obj);
-		}
-		return jarray;
-	}
 }
