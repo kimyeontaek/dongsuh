22d7ddcf3681f9b824975e54aeb34e8266f3fab2
LPS <LPS@DESKTOP-KGOL2UR>
2019-04-10 18:04:40 +0900

    test

--- src/main/java/com/ezen/controller/BasketController.java
+++ src/main/java/com/ezen/controller/BasketController.java
@@ -19,17 +19,17 @@ public class BasketController {
 	@Autowired
 	SqlSessionTemplate sqlSession;
 
-	/* 선택 삭제시 장바구니 페이지로 다시 이동 */
+
 	@RequestMapping(value = "/basket_check.do", method = RequestMethod.POST)
 	public String basket_check(BasketVO vo) {
 		String page = "";
 		int result = 0;
 		BasketDAO dao = sqlSession.getMapper(BasketDAO.class);
-		// 선택 삭제
+		// �꽑�깮 �궘�젣
 		for(int i=0; i<vo.getBasketCheckbox().length; i++){
 			result = dao.getSelectDeleteResult(vo.getBasketCheckbox()[i]);
 		}
-		// 선택 삭제 이후 basket.do를 다시 불러온다
+		// �꽑�깮 �궘�젣 �씠�썑 basket.do瑜� �떎�떆 遺덈윭�삩�떎
 		if (result == 1) {
 			page = "redirect:/basket.do";
 		}
@@ -37,14 +37,14 @@ public class BasketController {
 		return page;
 	}
 
-	/* 주문내역으로 이동 */
+	/* 二쇰Ц�궡�뿭�쑝濡� �씠�룞 */
 	@RequestMapping(value = "/order_list.do", method = RequestMethod.GET)
 	public ModelAndView basket(HttpSession session) {
 		ModelAndView mv = new ModelAndView();
 		String sid = (String) session.getAttribute("sid");
 
 		BasketDAO dao = sqlSession.getMapper(BasketDAO.class);
-		// 주문내역 보여주기
+		// 二쇰Ц�궡�뿭 蹂댁뿬二쇨린
 		ArrayList<BasketVO> list = dao.getOrderListResult(sid);
 
 		mv.addObject("list", list);
@@ -53,7 +53,7 @@ public class BasketController {
 		return mv;
 	}
 
-	/* 상품을 주문 햇을 때 */
+	/* �긽�뭹�쓣 二쇰Ц �뻼�쓣 �븣 */
 	@RequestMapping(value = "/basket_order_list.do", method = RequestMethod.POST)
 	public ModelAndView basket_order_list(BasketVO vo, HttpSession session) {
 		ModelAndView mv = new ModelAndView();
@@ -61,15 +61,15 @@ public class BasketController {
 		int result = 0;
 
 		BasketDAO dao = sqlSession.getMapper(BasketDAO.class);
-		// 주문내역 테이블에 추가
+		// 二쇰Ц�궡�뿭 �뀒�씠釉붿뿉 異붽�
 		for(int i=0; i<vo.getHname().length; i++){
 			result = dao.getOrderList(sid, vo.getHname()[i], vo.getHnum()[i], vo.getHprice()[i]);
 		}
-		// 기존 장바구니 목록 삭제
+		// 湲곗〈 �옣諛붽뎄�땲 紐⑸줉 �궘�젣
 		dao.getTruncate();
-		// 주문내역 목록 보여주기
+		// 二쇰Ц�궡�뿭 紐⑸줉 蹂댁뿬二쇨린
 		ArrayList<BasketVO> list = dao.getOrderListResult(sid);
-		
+
 		if (result == 1) {
 			mv.addObject("list", list);
 			mv.setViewName("/mypage/order_list");
